1a1f63aec17d7a82efd793076d493c341d2f8d03
Gabriel Molin <molinguin@gmail.com>
2024-03-10 13:28:09 -0300

    backend/ feat: populate relationships and fixing params

--- backend/src/core/utils/pagination.utils.ts
+++ backend/src/core/utils/pagination.utils.ts
@@ -1,14 +1,15 @@
 import { BadGatewayException } from "@nestjs/common";
-import { Query, Model } from "mongoose";
+import { Model } from "mongoose";
 
 export interface IPaginationParams{
     limit:number,
     page:number,
     count?:number,
+    populate?: string[]
 }
 
 enum queryParamsEnum {
-    "limit", "page"
+    "limit", "page", "populate"
 }
 
 export class PaginationUtils<T>{
@@ -24,18 +25,19 @@ export class PaginationUtils<T>{
         this.params = params;
         const found:T[] = await this.model
             .find()
-            .skip((params.page - 1) * params.limit)
-            .limit(params.limit);
+            .skip((this.params.page - 1) * params.limit)
+            .limit(this.params.limit)
+            .populate(this.params.populate[0] || "")
+            .populate(this.params.populate[1] || "")
 
             return {
                 pagination: {
-                    ...params,
+                    ...this.params,
                     count: found.length
                 }, 
                 result: found,
-              }
+            }
     }
-
 
 }
 
@@ -44,7 +46,7 @@ export function paginationParamsDto(params:IPaginationParams){
     if(
         Number.isNaN(parseInt(`${params.page}`))  || 
         Number.isNaN(parseInt(`${params.limit}`))
-    ) throw new BadGatewayException("Parametros incorretos");
+    ) throw new BadGatewayException("Incorrect query params!");
 
     let obj = {
         page: 1, limit: 10
@@ -52,10 +54,9 @@ export function paginationParamsDto(params:IPaginationParams){
 
     Object.entries(params).forEach(([key, value])=>{
         if(key in queryParamsEnum) obj[key] = value;
-    })
+    })    
+    if(!params.populate) Object.assign(obj, {populate : []})    
 
-    console.log(obj);
-    
     return obj;
 
 }
