106433163b5a4af35783a0e4689ab3c2c757b01e
Gabriel Molin <molinguin@gmail.com>
2024-03-11 00:07:20 -0300

    backend/ feat: attributes parseMod

--- backend/src/core/utils/attributes.utils.ts
+++ backend/src/core/utils/attributes.utils.ts
@@ -1,3 +1,5 @@
+import { BadRequestException } from "@nestjs/common";
+
 export enum attributesEnum{
     "strength",
     "dexterity",
@@ -23,6 +25,25 @@ export const attributeSet = {
     "intelligence": 0,
     "wisdom": 0,
     "charisma": 0, 
+}
+
+export function parseMod(attr:number):number {
+    if(attr >= 0 && attr < 8){
+        return -2;
+    } else if (attr >= 9 && attr <= 10){
+        return -1;
+    } else if (attr >= 11 && attr <= 12){
+        return 0;
+    } else if (attr >= 13 && attr <= 15){
+        return 1;
+    } else if (attr >= 16 && attr <= 18){
+        return 2;
+    } else if (attr >= 19 && attr <= 20){
+        return 3;
+    } else {
+        throw new BadRequestException("Maximum attribute set is 20")
+    }
+
 }
 
 export function getAttributesArray(): attributesEnum[]{
