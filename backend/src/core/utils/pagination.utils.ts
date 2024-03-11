cc893240222e4055e4bc3960d32b56c457ff9581
Gabriel Molin <molinguin@gmail.com>
2024-03-11 00:07:51 -0300

    backend/ feat: implement filter

--- backend/src/core/utils/pagination.utils.ts
+++ backend/src/core/utils/pagination.utils.ts
@@ -1,21 +1,22 @@
-import { BadGatewayException } from "@nestjs/common";
+import { BadGatewayException, NotFoundException } from "@nestjs/common";
 import { Model } from "mongoose";
 
 export interface IPaginationParams{
     limit:number,
     page:number,
     count?:number,
-    populate?: string[]
+    populate?: string[],
+    filter?: string,
+    deleted?: string,
 }
 
 enum queryParamsEnum {
-    "limit", "page", "populate"
+    "limit", "page", "populate", "filter", "deleted"
 }
 
 export class PaginationUtils<T>{
     private model:Model<T>;
-    private params:IPaginationParams
-    private result: T[]
+    private params:IPaginationParams;
     constructor(model:Model<T>, params?:IPaginationParams){
         this.model = model;
         if(params) this.params = params;
@@ -23,23 +24,54 @@ export class PaginationUtils<T>{
 
     async paginate(params:IPaginationParams){
         this.params = params;
+        const count = await this.model.countDocuments();        
+        let filter = {deleted: null};
+        if(params.filter){
+            const formatedFilters = this.parseFilter(params.filter);
+            filter = { ...filter, ...formatedFilters};
+        }
+        if(params.deleted === "true"){
+            filter.deleted = true;
+        }
+        
         const found:T[] = await this.model
-            .find()
+            .find(filter)
             .skip((this.params.page - 1) * params.limit)
             .limit(this.params.limit)
             .populate(this.params.populate[0] || "")
-            .populate(this.params.populate[1] || "")
+            .populate(this.params.populate[1] || "");
 
-            return {
-                pagination: {
-                    ...this.params,
-                    count: found.length
-                }, 
-                result: found,
+        return {
+            pagination: {
+                ...this.params,
+                count: found.length,
+                totalCount: count,
+                totalPages: Math.ceil(count / this.params.limit),
+                filterObj: filter
+            }, 
+            result: found,
+        };
+    }
+
+    private parseFilter(filters:string){
+        const splitted$:string[] = filters.split("$");        
+        const obj:any = {};
+        splitted$.forEach(filter => {
+            
+            if(filter === "heroes"){
+                obj.deleted = true;
+                return;
             }
+            const splitedFilter = filter.split(":");
+            if(splitedFilter.length < 2) throw new NotFoundException("Invalid URL filter format");
+            obj[splitedFilter[0]] = splitedFilter[1];
+        });
+        
+        return obj;
     }
 
 }
+
 
 export function paginationParamsDto(params:IPaginationParams){
     if(isObjectEmpty(params)) params = {limit:10, page:1};
